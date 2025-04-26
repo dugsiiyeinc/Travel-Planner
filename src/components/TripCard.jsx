a26517abb03b50d0f8c0ecd0b47f599208a6aaa9
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-26 20:32:27 +0300

    feat(TripCard): add date formatting and arrow icon

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { format } from "date-fns";
+import { ArrowRight } from "lucide-react"; // Import the ArrowRight icon from Lucide React
 
 
 const TripCard = () => {
