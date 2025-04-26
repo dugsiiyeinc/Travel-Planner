59957480ef87d2cf369d39dcf3bc9bffb51c51f2
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-26 22:46:36 +0300

    feat(blogPackGuide):  create basic BlogPackingGuide structure

---
+++ src/pages/BlogPackingGuide.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogPackingGuide = () => {
+  return (
+    <div className="bg-[#0E0F2C] text-white min-h-screen">
+      {/* Content coming soon */}
+    </div>
+  );
+};
+
+export default BlogPackingGuide;
