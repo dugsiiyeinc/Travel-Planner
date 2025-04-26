3d7f53b7050b572b49600e8fb231a55b632d566e
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-26 22:48:03 +0300

    feat(blogPackGuide): added an integrate navbar component

--- src/pages/BlogPackingGuide.jsx
+++ src/pages/BlogPackingGuide.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BlogPackingGuide = () => {
   return (
     <div className="bg-[#0E0F2C] text-white min-h-screen">
-      {/* Content coming soon */}
+       <Navbar />
     </div>
   );
 };
