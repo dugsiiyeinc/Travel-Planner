273a6f6d3a724db929575c8cc103d830480e54f2
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 09:21:19 +0300

    feat(blogEuropeanCities): Setup main content layout with max width, padding and margin

--- src/pages/BlogEuropeanCities.jsx
+++ src/pages/BlogEuropeanCities.jsx
@@ -1,8 +1,15 @@
+import Navbar from "../components/Navbar";
+
 const BlogEuropeanCities = () => {
   return (
     <div className="bg-[#0E0F2C] text-white min-h-screen">
-      <Navbar />
-      <main className="pt-20"></main>
+      <Navbar/>
+      <main className="pt-20">
+      <div className="max-w-4xl mx-auto px-6 py-12">
+        
+      </div>
+
+      </main>
     </div>
   );
 };
