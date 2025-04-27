8c27f2f47d4214759901375d6abc685c3d64650e
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 10:23:54 +0300

    feat(TripContext): imported a usestate hook

--- src/context/TripContext.jsx
+++ src/context/TripContext.jsx
@@ -1,9 +1,12 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 const TripContext = createContext();
 
 const TripProvider = ({ children }) => {
+  const [trips, setTrips] = useState([]);
   return <TripContext.Provider value={{}}>{children}</TripContext.Provider>;
 };
+
+// src/context/TripContext.js
 
 export { TripContext, TripProvider };
