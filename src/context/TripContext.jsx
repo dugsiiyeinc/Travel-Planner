6eeade4b0e7644ff37004ffa145c8249047d3228
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 10:26:51 +0300

    feat(TripContext): imported a useEffect hook

--- src/context/TripContext.jsx
+++ src/context/TripContext.jsx
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const TripContext = createContext();
 
 const TripProvider = ({ children }) => {
   const [trips, setTrips] = useState([]);
+  useEffect(() => {
+    const savedTrips = localStorage.getItem('trips');
+    if (savedTrips) {
+      setTrips(JSON.parse(savedTrips));
+    } else {
+      setTrips(sampleTrips);
+      localStorage.setItem('trips', JSON.stringify(sampleTrips));
+    }
+  }, []);
   return <TripContext.Provider value={{}}>{children}</TripContext.Provider>;
 };
 
