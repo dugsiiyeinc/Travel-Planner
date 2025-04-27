dda9bea6ce414e9d2afa0dfeafee14f3d9cb28e4
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 09:19:55 +0300

    feat(blogEuropeanCities): Add integrate navbar component

--- src/pages/BlogEuropeanCities.jsx
+++ src/pages/BlogEuropeanCities.jsx
@@ -1,14 +1,10 @@
-
-
-
 const BlogEuropeanCities = () => {
-    return (
-      <div className="bg-[#0E0F2C] text-white min-h-screen">
-        <main className="pt-20">
-        </main>
-      </div>
-    );
-  };
-  
-  export default BlogEuropeanCities;
-  
+  return (
+    <div className="bg-[#0E0F2C] text-white min-h-screen">
+      <Navbar />
+      <main className="pt-20"></main>
+    </div>
+  );
+};
+
+export default BlogEuropeanCities;
