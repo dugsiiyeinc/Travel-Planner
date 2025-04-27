01d4184f0b9f324db9744633483971dcf9d3e64f
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 10:32:25 +0300

    feat(TripContext): add an addTrip function to update a trip in the list

--- src/context/TripContext.jsx
+++ src/context/TripContext.jsx
@@ -1,19 +1,27 @@
 import React, { createContext, useEffect, useState } from "react";
-import sampleTrips from '../data/trips.json';
+import sampleTrips from "../data/trips.json";
 
 const TripContext = createContext();
 
 const TripProvider = ({ children }) => {
   const [trips, setTrips] = useState([]);
   useEffect(() => {
-    const savedTrips = localStorage.getItem('trips');
+    const savedTrips = localStorage.getItem("trips");
     if (savedTrips) {
       setTrips(JSON.parse(savedTrips));
     } else {
       setTrips(sampleTrips);
-      localStorage.setItem('trips', JSON.stringify(sampleTrips));
+      localStorage.setItem("trips", JSON.stringify(sampleTrips));
     }
   }, []);
+
+  // src/context/TripContext.js
+  const addTrip = (newTrip) => {
+    const updatedTrips = [...trips, newTrip];
+    setTrips(updatedTrips);
+    localStorage.setItem("trips", JSON.stringify(updatedTrips));
+  };
+
   return <TripContext.Provider value={{}}>{children}</TripContext.Provider>;
 };
 
