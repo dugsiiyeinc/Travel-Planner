36f4e1e2cedb6864e6fff7808ee3ff54aaae8c82
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 19:52:48 +0300

    feat(BlogBudgetTravel): Import Navlink from react-router-dom

--- src/pages/BlogBudgetTravel.jsx
+++ src/pages/BlogBudgetTravel.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { NavLink } from "react-router-dom";
+
 
 const BlogBudgetTravel = () => {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12">
       <Navbar/>
-      <main className="">
+      <main className="pt-20">
+        <div className="max-w-4xl mx-auto px-6 py-12">
 
+        </div>
       </main>
     </div>
   );
