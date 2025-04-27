a3af3417a5da3bab29dc2ff7fa8ad88bf3638557
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 09:15:53 +0300

    feat(blogEuropeanCities): Create initial BlogEuropeanCities page with basic structure

---
+++ src/pages/BlogEuropeanCities.jsx
@@ -0,0 +1,14 @@
+
+
+
+const BlogEuropeanCities = () => {
+    return (
+      <div className="bg-[#0E0F2C] text-white min-h-screen">
+        <main className="pt-20">
+        </main>
+      </div>
+    );
+  };
+  
+  export default BlogEuropeanCities;
+  
