28f41998a3e4eccf0e31842a986897d61619a251
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-04-27 19:39:12 +0300

    feat(BlogBudgetTravel): Import and Add navbar component to page

--- src/pages/BlogBudgetTravel.jsx
+++ src/pages/BlogBudgetTravel.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const BlogBudgetTravel = () => {
-  return <div className="max-w-4xl mx-auto px-6 py-12">BlogBudgetTravel</div>;
+  return (
+    <div className="max-w-4xl mx-auto px-6 py-12">
+      <Navbar/>
+    </div>
+  );
 };
 
 export default BlogBudgetTravel;
